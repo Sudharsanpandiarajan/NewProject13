a934b2af37e261dd1b544f698f818974eecbc7af
Sudharsanpandiarajan <mr.sudharsanofficial@gmail.com>
2022-11-28 16:42:11 +0530

    code has updated

--- src/org/sample/Coding.java
+++ src/org/sample/Coding.java
@@ -1,5 +1,19 @@
 package org.sample;
 
 public class Coding {
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println(1);
+		System.out.println(2);
+		System.out.println(3);
+		System.out.println(4);
+		
+		
+		
+	}
+	
+	
 
 }
